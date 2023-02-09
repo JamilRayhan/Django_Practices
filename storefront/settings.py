fbeab607096735b91703577ef2a84cb5278fbb3e
JamilRayhan <jamilrayhan.cse@gmail.com>
2023-02-10 01:00:02 +0600

    9.2.23

--- storefront/settings.py
+++ storefront/settings.py
@@ -116,11 +116,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_ROOT=   r'C:\Users\jamil\OneDrive\Desktop\Django_Practices'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+print(STATIC_DIR)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- storefront/urls.py
+++ storefront/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from playground import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playground/',include('playground.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
