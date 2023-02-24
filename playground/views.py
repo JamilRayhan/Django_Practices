f4e4966b2746a0eaf651fce66bb2a3a4db66056b
JamilRayhan <jamilrayhan.cse@gmail.com>
2023-02-24 20:40:57 +0600

    new

--- playground/views.py
+++ playground/views.py
@@ -5,7 +5,8 @@ from playground.models import Musician,Album
 from playground import forms
 
 def index(request):
-    diction = {"title":"Home Page"}
+    musician_list = Musician.objects.order_by('first_name')
+    diction = {"title":"Home Page",'musician_list':musician_list}
     return render(request,'playground/index.html',context=diction)
 
 
@@ -14,9 +15,24 @@ def album_list(request):
     return render(request,'playground/album_list.html',context=diction)
 
 def musician_form(request):
-    diction = {'title':"Add Musician"}
+    form=forms.MusicianForm()
+    
+    if request.method=="POST":
+        form = forms.MusicianForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+    
+    diction = {'title':"Add Musician",'musician_form':form}
     return render(request,'playground/musician_form.html',context=diction)
 
 def album_form(request):
-    diction = {'title':"Add Album"}
+    form = forms.AlbumForm()
+    
+    if request.method=="POST":
+        form = forms.AlbumForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+    diction = {'title':"Add Album",'album_form':form}
     return render(request,'playground/album_form.html',context=diction)
