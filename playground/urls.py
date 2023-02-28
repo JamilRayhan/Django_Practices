1ea9690523ad0fbedbc17a4b357ee6a682969e3d
JamilRayhan <jamilrayhan.cse@gmail.com>
2023-03-01 01:42:50 +0600

    update and delete

--- playground/urls.py
+++ playground/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('add_album/',views.album_form,name='album_form'),
     path('add_musician/',views.musician_form,name='musician_form'),
-    path('album_list/<int:id>',views.album_list,name='album_list'),
-    path('edit_artist/<int:id>',views.edit_artist,name='edit_artist'),
+    path('album_list/<int:id>/',views.album_list,name='album_list'),
+    path('edit_artist/<int:id>/',views.edit_artist,name='edit_artist'),
+    path('edit_album/<int:id>/',views.edit_album,name='edit_album'),
+    path('delete_album/<int:id>/',views.delete_album,name='delete_album'),
+    path('delete_musician/<int:id>/',views.delete_musician,name='delete_musician'),
+    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- playground/views.py
+++ playground/views.py
@@ -45,5 +45,35 @@ def album_form(request):
 def edit_artist(request, id):
     artist_info = Musician.objects.get(pk=id)
     form=forms.MusicianForm(instance=artist_info)
+    
+    if request.method =='POST':
+        form = forms.MusicianForm(request.POST,instance=artist_info)
+        if form.is_valid():
+            form.save(commit=True)
+            return album_list(request,id)
     diction ={'edit_form':form}
     return render(request,'playground/edit_artist.html',context=diction)
+
+def edit_album(request,id):
+    album_info = Album.objects.get(pk=id)
+    form=forms.AlbumForm(instance=album_info)
+    diction={}
+    if request.method =='POST':
+        form = forms.AlbumForm(request.POST,instance=album_info)
+        if form.is_valid():
+            form.save(commit=True)
+            diction.update({'success_text':"Successfully Updated!"})
+    
+    diction.update({'edit_form':form})
+    diction.update({'album_id':id})
+    return render(request,'playground/edit_album.html',context=diction)
+
+def delete_album(request,id):
+    album =Album.objects.get(pk=id).delete()
+    diction={'delete_success':"Album Deleted Successfully!"}
+    return render(request,'playground/delete.html',context=diction)
+
+def delete_musician(request,id):
+    album =Musician.objects.get(pk=id).delete()
+    diction={'delete_success':"Musician Deleted Successfully!"}
+    return render(request,'playground/delete.html',context=diction)
