a3a70d723f6b87421994b5c88b6af54709c9fcfa
JamilRayhan <jamilrayhan.cse@gmail.com>
2023-03-07 22:58:49 +0600

    ok

--- storefront/settings.py
+++ storefront/settings.py
@@ -129,7 +129,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-print(STATIC_DIR)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
